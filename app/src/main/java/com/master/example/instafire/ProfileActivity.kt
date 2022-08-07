ff6177fe54cf1e7a1ae93875a861166a1c03d63f
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-08-07 17:58:41 +0200

    added profile activity and logout functionality

--- app/src/main/java/com/master/example/instafire/ProfileActivity.kt
+++ app/src/main/java/com/master/example/instafire/ProfileActivity.kt
@@ -3,25 +3,31 @@ package com.master.example.instafire
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
-class PostsActivity : AppCompatActivity() {
+private const val TAG = "ProfileActivity"
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posts)
+        setContentView(R.layout.activity_profile)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_posts, menu)
+        menuInflater.inflate(R.menu.menu_profile, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_profile) {
-            val intent = Intent(this, ProfileActivity::class.java)
+        if (item.itemId == R.id.menu_logout) {
+            Log.i(TAG, "User wants to logout")
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
         return super.onOptionsItemSelected(item)
     }
 }

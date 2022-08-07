6cdefb910d3a19d4846145a109034f78d32cb6a0
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-08-07 15:04:11 +0200

    added firebase authentication to the project

--- app/src/main/java/com/master/example/instafire/LoginActivity.kt
+++ app/src/main/java/com/master/example/instafire/LoginActivity.kt
@@ -2,10 +2,40 @@ package com.master.example.instafire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Email/Password can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            // Firebase auth check
+            val auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                    goPostsActivity()
+                } else {
+                    Log.i(TAG, "signInWithEmailFailed", task.exception)
+                    Toast.makeText(this, "Authentication failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun goPostsActivity() {
+        Log.i(TAG, "goPostsActivity")
     }
 }

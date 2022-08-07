5b72eb0dfda788eb87479a700d37bdee56d24caf
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-08-07 19:06:04 +0200

    added post and user entities at firestore storage and db and fetching them by query

--- app/src/main/java/com/master/example/instafire/PostsActivity.kt
+++ app/src/main/java/com/master/example/instafire/PostsActivity.kt
@@ -3,13 +3,40 @@ package com.master.example.instafire
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.master.example.instafire.models.Post
+
+private const val TAG = "PostsActivity"
 
 class PostsActivity : AppCompatActivity() {
+
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        firestore = FirebaseFirestore.getInstance()
+        val postsReference = firestore
+            .collection("posts")
+            .limit(20)
+            .orderBy("creation_time_ms",Query.Direction.DESCENDING)
+
+        postsReference.addSnapshotListener { snapshot, exception ->
+            if (exception != null || snapshot == null) {
+                Log.i(TAG, "Exception when querying posts", exception)
+                return@addSnapshotListener
+            }
+            val postList = snapshot.toObjects(Post::class.java)
+            for (post in postList) {
+                Log.i(TAG, "Post ${post}")
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/com/master/example/instafire/models/User.kt
@@ -0,0 +1,3 @@
+package com.master.example.instafire.models
+
+data class User(var userName: String = "", var age: Int = 0)

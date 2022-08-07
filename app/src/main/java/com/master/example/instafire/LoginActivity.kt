e015d36d584d8062faceb1796d4763b4ecd4c230
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-08-07 15:23:34 +0200

    added new posts activity in case user logged in succesfully and fixed some bugs

--- app/src/main/java/com/master/example/instafire/LoginActivity.kt
+++ app/src/main/java/com/master/example/instafire/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.master.example.instafire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,7 +15,15 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val auth = FirebaseAuth.getInstance()
+        // we are checking here to because if the user is already logged in we don't want to see login activity and to be logged in again
+        if (auth.currentUser != null) {
+            Log.i(TAG, "User is already logged in!")
+            goPostsActivity()
+        }
         btnLogin.setOnClickListener {
+            // we are disabling the logging button because in case it's been pushed multiple times our posts activity were getting opened multiple times
+            btnLogin.isEnabled = false
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
             if (email.isBlank() || password.isBlank()) {
@@ -22,8 +31,8 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
             // Firebase auth check
-            val auth = FirebaseAuth.getInstance()
             auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                btnLogin.isEnabled = true
                 if (task.isSuccessful) {
                     Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
                     goPostsActivity()
@@ -37,5 +46,9 @@ class LoginActivity : AppCompatActivity() {
 
     private fun goPostsActivity() {
         Log.i(TAG, "goPostsActivity")
+        val intent = Intent(this, PostsActivity::class.java)
+        startActivity(intent)
+        // we added finish() method so when the back button clicked it doesn't return back to the login page again
+        finish()
     }
 }
---
+++ app/src/main/java/com/master/example/instafire/PostsActivity.kt
@@ -0,0 +1,11 @@
+package com.master.example.instafire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts)
+    }
+}
